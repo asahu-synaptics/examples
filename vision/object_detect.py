c6041f6867971a25a2e193176c812a42baef00f6
Dominic Pajak <dominic.pajak@synaptics.com>
2025-02-24 21:00:06 +0000

    Init

---
+++ vision/object_detect.py
@@ -0,0 +1,26 @@
+
+from synapRT.pipelines import pipeline
+from examples.utils.websockets import WebSockets
+import json
+import sys
+
+def main():
+    ws_server = WebSockets(port=6789)
+    ws_server.start()
+
+    def handle_results(results, inference_time):
+        message = json.dumps(results)
+        ws_server.broadcast(message)
+
+    pipe = pipeline(
+        task="object-detection",
+        model="/usr/share/synap/models/object_detection/coco/model/yolov8s-640x384/model.synap",
+        profile=True,
+        handler=handle_results,
+    )
+
+    print("Starting Object Dectection Stream.")
+    pipe(sys.argv[1])
+
+if __name__ == "__main__":
+    main()
