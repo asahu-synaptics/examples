939019e8af985aac591eef9569ed796479232335
Sauryadeep Pal <sauryadeep.pal@synaptics.com>
2025-03-03 17:22:19 -0800

    Add code for auto-detecting camera (#8)

--- utils/photo.py
+++ utils/photo.py
@@ -1,6 +1,51 @@
 import subprocess
+import warnings
 
-def capture_photo(device="/dev/video7", filename="out.jpg"):
+try:
+    import gi
+    gi.require_version("GUdev", "1.0")
+    from gi.repository import GUdev
+    GLIB_AVAILABLE = True
+except ImportError:
+    GLIB_AVAILABLE = False
+    warnings.warn("Unable to import gi, camera detection defaulting to /dev/video7")
+
+def get_camera_devices(cam_subsys: str = "video4linux") -> list[str]:
+    if not GLIB_AVAILABLE:
+        return ["/dev/video7"]
+
+    camera_paths: list[str] = []
+    client = GUdev.Client(subsystems=[cam_subsys])
+    devices = client.query_by_subsystem(cam_subsys)
+
+    for device in devices:
+        bus = device.get_property("ID_BUS")
+        if bus == "usb":
+            sys_path = device.get_sysfs_path()
+            if sys_path:
+                index_path = f"{sys_path}/index"
+                try:
+                    with open(index_path, "r") as f:
+                        contents = f.read().strip()
+                        index_val = int(contents)
+
+                        if index_val == 0:
+                            dev_node = device.get_device_file()
+                            if dev_node:
+                                camera_paths.append(dev_node)
+                except OSError as e:
+                    warnings.warn(f"Warning: Error reading {index_path}: {e}")
+                except ValueError:
+                    warnings.warn(f"Warning: Unexpected contents in {index_path}: {contents}")
+
+    return camera_paths
+
+def capture_photo(device=None, filename="out.jpg"):
+    try:
+        device = device or get_camera_devices()[0]
+    except IndexError:
+        warnings.warn("Valid camera device not found, defaulting to /dev/video7")
+        device = "/dev/video7"
     # Set the video format to MJPG at 640x480.
     fmt_cmd = [
         "v4l2-ctl",
